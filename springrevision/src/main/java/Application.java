00e8944366ff3f5c9c9c85f7159b25b4b257501f
navspeak <navneet.sahay@gmail.com>
2020-05-17 22:26:05 -0400

    XML Configuration and autowire

---
+++ springrevision/src/main/java/Application.java
@@ -0,0 +1,11 @@
+import com.nav.spring.service.SpeakerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpeakerService speakerService = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println((speakerService.findAll().get(0).getFirstName()));
+    }
+}
--- springrevision/src/main/java/com/nav/spring/service/SpeakerServiceImpl.java
+++ springrevision/src/main/java/com/nav/spring/service/SpeakerServiceImpl.java
@@ -8,7 +8,18 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository speakerRepository = new HibernateSpeakerRepositoryImpl();
+
+    public SpeakerServiceImpl() {}
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
+
     public List<Speaker> findAll() {
         return speakerRepository.findAll();
     }
+
+//    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+//        this.speakerRepository = speakerRepository;
+//    }
 }
